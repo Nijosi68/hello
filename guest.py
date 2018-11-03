85be7717a7ba8b4c56f89666b3c7e3164980fd1a
Nijosi68 <38964414+Nijosi68@users.noreply.github.com>
2018-11-03 23:41:57 +0000

    Latest appends

--- guest.py
+++ guest.py
@@ -6,9 +6,11 @@ event_details ={
     'venue2': 'the club'
 }
 
-message = 'Hi ' + guest_list[0] + '! I am holding a dinner party on %i %s' %(event_details['date'], 
+message = 'Hi ' + guest_list[0] + '! I am holding a dinner party on %i %s' %(event_details['date'],
 event_details['month']) + ' at %s' %(event_details['venue1']) + '. I hope you can come!'
 print(message)
 print(guest_list)
 guest_list.append('Michael')
+print(guest_list)
+guest_list.append('Shane')
 print(guest_list)
